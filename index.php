f7ddd451d4e0231109cbe013dea13b28a2c5e3d8
wave kurb <dis-students@dexon-technology.com>
2025-04-23 14:47:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CPOC AIMS - Asset Integrity Management</title>
-
+kkkkk
     <link rel="stylesheet" href="css/bootstrap.css" >
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" >
     <link rel="stylesheet" type="text/css" href="https://cdn3.devexpress.com/jslib/20.1.7/css/dx.common.css" />
